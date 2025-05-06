0c8db2486adcd2073d3307f532cd801c502b58b2
Giancarlo Rosa <giannesdabrun@hotmail.com>
2025-05-05 23:36:11 -0300

    add user entity tests and refactor

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,7 +33,8 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/unbound-method': 'off'
+      '@typescript-eslint/unbound-method': 'off',
+      "@typescript-eslint/no-namespace": 'off'
     },
   },
 );
--- src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -1,13 +1,32 @@
 import { ConflictError } from '@/shared/domain/errors/conflict-error';
 import { NotFoundError } from '@/shared/domain/errors/not-found-error';
 import { InMemorySearchableRepository } from '@/shared/domain/repositories/in-memory-searchable-repository';
+import { SortDirection } from '@/shared/domain/repositories/searchable-repository-contracts ';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 
 export class UserInMemoryRepository
   extends InMemorySearchableRepository<UserEntity>
-  implements UserRepository
+  implements UserRepository.Repository
 {
+  protected async applyFilter(
+    items: UserEntity[],
+    filter: UserRepository.Filter | null,
+  ): Promise<UserEntity[]> {
+    if (!filter) {
+      return items;
+    }
+
+    return new Promise(resolve =>
+      resolve(
+        items.filter(item =>
+          item.props.name.toLowerCase().includes(filter.toLowerCase()),
+        ),
+      ),
+    );
+  }
+  sortableFields: string[] = ['name', 'createdAt'];
+
   async findByEmail(email: string): Promise<UserEntity> {
     const entity: UserEntity = await new Promise(resolve =>
       resolve(this.items.find(item => item.email === email)),
@@ -28,5 +47,15 @@ export class UserInMemoryRepository
     if (entity) {
       throw new ConflictError('Email address already used');
     }
+  }
+
+  protected async applySort(
+    items: UserEntity[],
+    sort: string | null,
+    sortDir: SortDirection | null,
+  ): Promise<UserEntity[]> {
+    return !sort
+      ? super.applySort(items, 'createdAt', 'desc')
+      : super.applySort(items, sort, sortDir);
   }
 }
